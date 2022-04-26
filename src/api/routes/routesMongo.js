2d4609ffd52493ecdc48f6d68a9043279a258f0f
Nahir Massetani Maidanik <nanu-@live.com>
2022-04-26 19:34:24 -0300

    routes y controllers Mongo

---
+++ src/api/routes/routesMongo.js
@@ -0,0 +1,26 @@
+const express =  require('express')
+const routesProducts = express.Router()
+const routesCart = express.Router()
+
+const productsControllers = require('../controllers/products/productsControllerMongo')
+const cartsControllers = require('../controllers/cart/cartControllersMongo')
+
+const admin = require('../middleware/admin')
+const adminRol = true
+
+//RUTAS PRODUCTOS
+routesProducts.get('/', productsControllers.getAllProducts)
+routesProducts.get('/:id', productsControllers.getProductById)
+routesProducts.post('/', admin(adminRol), productsControllers.addProduct)
+routesProducts.put('/:id', admin(adminRol), productsControllers.updateProduct)
+routesProducts.delete('/:id', admin(adminRol), productsControllers.deleteProduct)
+
+//RUTAS CARRITOS
+routesCart.post('/', cartsControllers.addCart)
+routesCart.delete('/:id', cartsControllers.deleteCart)
+routesCart.get('/:id/products', cartsControllers.productsinCart)
+routesCart.post('/:id/products', cartsControllers.addProductInCart)
+routesCart.delete('/:idcart/products/:idprod', cartsControllers.deleteProductInCart)
+
+
+module.exports = { routesProducts, routesCart }
