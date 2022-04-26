8112f42bca702e9f79e72e307a145cd6cb6ac25a
Nahir Massetani Maidanik <nanu-@live.com>
2022-04-26 13:49:56 -0300

    controller y routes products FS

--- src/api/routes/routesProducts.js
+++ src/api/routes/routesProducts.js
@@ -1,60 +1,20 @@
 const express =  require('express')
 const routesProducts = express.Router()
 
-const containerProducts = require('../daos/products/productsDaosFS')
-const newContainerProducts = new containerProducts()
+const productsControllers = require('../controllers/products/productsControllerFS')
 
 const admin = require('../middleware/admin')
 const adminRol = true
 
-routesProducts.get('/', async (req, res) => {
-    try {
-        const allProducts =  await newContainerProducts.getAllProducts()
-        res.status(200).json(allProducts)
-    }catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+routesProducts.get('/', productsControllers.getAllProducts)
 
-routesProducts.get('/:id', async (req, res) => {
-    try {
-        const productById = await newContainerProducts.getByIdProduct(req.params.id)
-        if (productById != undefined) {
-            return res.status(200).json(productById)
-        } else {
-            return res.status(404).json({ error : 'Producto no encontrado' }) //ver si esto va
-        }
-    }catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+routesProducts.get('/:id', productsControllers.getProductById)
 
-routesProducts.post('/', admin(adminRol), async (req, res) => {
-    try {
-        const newProduct = await newContainerProducts.saveProduct(req.body) 
-        res.status(201).json(newProduct)
-    }catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+routesProducts.post('/', admin(adminRol), productsControllers.addProduct)
 
-routesProducts.put('/:id', admin(adminRol), async (req, res) => {
-    try {
-        const updateProduct = await newContainerProducts.updateProduct(req.body, req.params.id)
-        res.status(200).json(updateProduct)
-    }catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+routesProducts.put('/:id', admin(adminRol), productsControllers.updateProduct)
 
-routesProducts.delete('/:id', admin(adminRol), async (req, res) => {
-    try {
-        const deleteProduct = await newContainerProducts.deleteProduct(req.params.id)
-        res.status(200).json(deleteProduct)
-    }catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+routesProducts.delete('/:id', admin(adminRol), productsControllers.deleteProduct)
 
 module.exports = routesProducts
 
