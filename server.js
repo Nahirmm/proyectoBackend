040cf49cba0bc81a6cd105b2429fa9e5ba11ec99
Nahir Massetani Maidanik <nanu-@live.com>
2022-05-03 18:32:18 -0300

    configurado para que corra con mongoDB

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const app = require('./app')
-// const mongoose = require('mongoose')
 require('dotenv').config()
 
-const PORT = process.env.PORT;
-// const urlMongo= process.env.DB
-
-
-// mongoose.connect(urlMongo, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(data => console.log("Conectado a MongoDB"))
-
+const PORT = process.env.PORT
 
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`))
